0d8e5509db42b7783db11e14504dfb95c13f9005
Solomon Hawk <solomon.hawk@viget.com>
2024-08-15 23:09:23 -0400

    Add limited undo

--- src/components/game.tsx
+++ src/components/game.tsx
@@ -8,6 +8,8 @@ import {
   displayOperation,
   game$,
 } from "~/state/game";
+import { Loop } from "./loop";
+import { ResetIcon } from "@radix-ui/react-icons";
 
 export function Game() {
   return (
@@ -16,9 +18,10 @@ export function Game() {
         <OperationHeading />
 
         <div className="flex gap-1">
-          <For each={game$.columns} item={Column} optimized />
+          <For each={game$.state.columns} item={Column} optimized />
         </div>
 
+        <Undo />
         <MoveCount />
       </div>
     </div>
@@ -33,7 +36,7 @@ function OperationHeading() {
           if (game$.status.get() === "won") {
             return "You won!";
           } else {
-            return displayOperation(game$.currentOperation.get());
+            return displayOperation(game$.state.currentOperation.get());
           }
         }}
       </Computed>
@@ -45,11 +48,44 @@ function MoveCount() {
   return (
     <div className="text-center">
       <p className="font-bold">
-        Moves: <Memo>{game$.moveCount}</Memo>
+        Moves: <Memo>{game$.state.moveCount}</Memo>
       </p>
     </div>
   );
 }
+
+const Undo = observer(function Undo() {
+  const canUndo = game$.canUndo();
+  const undoCount = game$.undoCount.get();
+  const isWon = game$.status.get() === "won";
+
+  return (
+    <div className="flex gap-2 items-center justify-between">
+      <button
+        onClick={game$.undoPreviousOperation}
+        disabled={!canUndo || isWon}
+        className="text-zinc-100 enabled:hover:text-white enabled:hover:bg-zinc-900 enabled:active:bg-zinc-800 disabled:text-zinc-500 disabled:border-zinc-700 flex gap-1 items-center text-sm border border-zinc-500 rounded-md px-2 py-1 transition-colors"
+      >
+        <ResetIcon />
+        Undo
+      </button>
+
+      <div className="flex items-center gap-1">
+        <Loop times={game$.undoLimit.get()}>
+          {(i) => (
+            <div
+              key={i}
+              className={cx("size-4 rounded-full", {
+                "bg-green-500": i < undoCount,
+                "bg-zinc-700": i >= undoCount,
+              })}
+            />
+          )}
+        </Loop>
+      </div>
+    </div>
+  );
+});
 
 const Column = observer(function Column({
   item,
@@ -59,7 +95,7 @@ const Column = observer(function Column({
   const previewsEnabled = game$.previews.get();
   const dimensions = game$.dimensions.get();
   const filled = item.filled.get();
-  const currentOperation = game$.currentOperation.get();
+  const currentOperation = game$.state.currentOperation.get();
   const isWon = game$.status.get() === "won";
   const isEnabled =
     canApplyOperation(dimensions, item.get(), currentOperation) && !isWon;
@@ -73,8 +109,8 @@ const Column = observer(function Column({
         game$.applyCurrentOperation(item, nextColumnState);
       }}
     >
-      {new Array(dimensions.y).fill(0).map((_, i) => {
-        return (
+      <Loop times={dimensions.y}>
+        {(i) => (
           <div
             key={i}
             className={cx("size-8 rounded-sm", {
@@ -88,8 +124,8 @@ const Column = observer(function Column({
                   (i < filled && i >= nextColumnState.filled)),
             })}
           />
-        );
-      })}
+        )}
+      </Loop>
     </button>
   );
 });
